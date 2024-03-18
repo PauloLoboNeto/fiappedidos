f82cb3b9a241dd1324d3fb9287b829d446e6d8b9
gabriel <gabrielalmeidads@gmail.com>
2024-03-18 00:43:07 -0300

    FIX validação de CPF

--- src/main/java/com/fiap/pedidos/entities/Cpf.java
+++ src/main/java/com/fiap/pedidos/entities/Cpf.java
@@ -55,54 +55,7 @@ public final class Cpf {
     }
 
     public boolean isValid() {
-        // considera-se erro CPF's formados por uma sequencia de numeros iguais
-        Matcher matcher = REGEX_CPF_NUMERO_REPETIDO.matcher(getCpf());
-        if (matcher.find() || getCpf().length() != 11)
-            return(false);
-
-        char dig10, dig11;
-        int sm, i, r, num, peso;
-
-        // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
-        try {
-            // Calculo do 1o. Digito Verificador
-            sm = 0;
-            peso = 10;
-            for (i=0; i<9; i++) {
-                // converte o i-esimo caractere do CPF em um numero:
-                // por exemplo, transforma o caractere '0' no inteiro 0
-                // (48 eh a posicao de '0' na tabela ASCII)
-                num = (int)(getCpf().charAt(i) - 48);
-                sm = sm + (num * peso);
-                peso = peso - 1;
-            }
-
-            r = 11 - (sm % 11);
-            if ((r == 10) || (r == 11))
-                dig10 = '0';
-            else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
-
-            // Calculo do 2o. Digito Verificador
-            sm = 0;
-            peso = 11;
-            for(i=0; i<10; i++) {
-                num = (int)(getCpf().charAt(i) - 48);
-                sm = sm + (num * peso);
-                peso = peso - 1;
-            }
-
-            r = 11 - (sm % 11);
-            if ((r == 10) || (r == 11))
-                dig11 = '0';
-            else dig11 = (char)(r + 48);
-
-            // Verifica se os digitos calculados conferem com os digitos informados.
-            if ((dig10 == getCpf().charAt(9)) && (dig11 == getCpf().charAt(10)))
-                return(true);
-            else return(false);
-        } catch (InputMismatchException erro) {
-            return(false);
-        }
+        return Objects.nonNull(this.cpf) && (!this.cpf.isEmpty()) && (!this.cpf.isEmpty()) && (this.cpf.length() == 14 || this.cpf.length() == 11);
     }
 
 }
